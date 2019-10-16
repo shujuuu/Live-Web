800bbbed8353383a808d4d32dd725f5ef0b0f965
shujulin <43021463+shujuuu@users.noreply.github.com>
2019-10-16 02:39:35 -0400

    Add files via upload
    added server

---
+++ week6/server.js
@@ -0,0 +1,61 @@
+var https = require('https');
+var fs = require('fs'); // Using the filesystem module
+var url = require('url');
+
+var options = {
+    key: fs.readFileSync('my-key.pem'),
+    cert: fs.readFileSync('my-cert.pem')
+};
+
+function handleIt(req, res) {
+    var parsedUrl = url.parse(req.url);
+
+    var path = parsedUrl.pathname;
+    if (path == "/") {
+        path = "index.html";
+    }
+
+    fs.readFile(__dirname + path,
+
+        // Callback function for reading
+        function (err, fileContents) {
+            // if there is an error
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading ' + req.url);
+            }
+            // Otherwise, send the data, the contents of the file
+            res.writeHead(200);
+            res.end(fileContents);
+        }
+    );
+
+    // Send a log message to the console
+    console.log("Got a request " + req.url);
+}
+
+var httpServer = https.createServer(options, handleIt);
+httpServer.listen(9010);
+
+console.log('Server listening on port 9010');
+
+// WebSocket Portion
+// WebSockets work with the HTTP server
+var io = require('socket.io').listen(httpServer);
+var peers = [];
+
+io.sockets.on('connection',
+    // We are given a websocket object in our function
+    function (socket) {
+
+        console.log("We have a new client: " + socket.id);
+        socket.on('sendPoses', function (data) {
+            console.log(data);
+            socket.broadcast.emit('otherPoses', data);
+        });
+
+        socket.on('disconnect', function () {
+            console.log("Client has disconnected");
+        });
+    }
+);
