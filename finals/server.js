322fe30b862b245ebca2b513f789c342786231ca
shujulin <43021463+shujuuu@users.noreply.github.com>
2019-12-05 13:30:04 -0500

    Create server.js

---
+++ finals/server.js
@@ -0,0 +1,38 @@
+// HTTP Portion
+var http = require('http');
+var fs = require('fs');
+var httpServer = http.createServer(requestHandler);
+httpServer.listen(9000);
+console.log('listening 9000');
+
+function requestHandler(req, res) {
+    var parsedUrl = url.parse(req.url);
+    console.log("The Request is: " + parsedUrl.pathname);
+    fs.readFile(__dirname + parsedUrl.pathname,
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading ' + parsedUrl.pathname);
+            }
+            res.writeHead(200);
+            res.end(data);
+        }
+    );
+}
+
+// WebSocket Portion
+var io = require('socket.io').listen(httpServer);
+io.sockets.on('connection',
+    function (socket) {
+        console.log("We have a new client: " + socket.id);
+        socket.on('chatmessage', function (data) {
+            console.log("Received: 'chatmessage' " + data);
+            socket.broadcast.emit('chatmessage', data);
+            socket.broadcast.emit('chatmessage', socket.id);
+        });
+
+        socket.on('disconnect', function () {
+            console.log("Client has disconnected " + socket.id);
+        });
+    }
+);
